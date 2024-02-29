280955510a9fe5f3f11c6a46a85d08f39498a70a
Miguel Ángel Rodríguez <miguelangel.rodriguez.hernando@gmail.com>
2024-02-29 21:03:27 +0000

    Remove unused dependencies and plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
-import { astroImageTools } from 'astro-imagetools';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://portfolio.miguelo.dev/',
-  experimental: {
-    integrations: true,
-  },
   integrations: [
     tailwind({}),
-    sitemap(),
     robotsTxt(),
-    astroImageTools,
   ],
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [],
 };
