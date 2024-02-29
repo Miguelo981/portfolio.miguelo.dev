f62712498f5f27904cce908b0c038977b97676f8
Miguel Ángel Rodríguez <miguelangel.rodriguez.hernando@gmail.com>
2024-02-29 21:07:48 +0000

    Add @astrojs/vercel dependency and configure Astro for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import robotsTxt from 'astro-robots-txt';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://portfolio.miguelo.dev/',
+  output: 'hybrid',
   integrations: [
     tailwind({}),
     robotsTxt(),
   ],
+  adapter: vercel()
 });
