65e9e38ca20e896e60a8662daba84d7f8ba988b1
Miguel Ángel Rodríguez <miguelangel.rodriguez.hernando@gmail.com>
2024-11-06 13:35:27 +0000

    Update astro to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import robotsTxt from 'astro-robots-txt';
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://portfolio.miguelo.dev/',
-  output: 'hybrid',
+  output: 'static',
   integrations: [
     tailwind({}),
     robotsTxt(),
-  ],
-  adapter: vercel()
+  ]
 });
